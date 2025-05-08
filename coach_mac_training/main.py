0fbb9780b8f5e8dea100f3b0135a3fc5ec100b98
acobb <aecobb53@gmail.com>
2025-05-08 12:13:37 -0600

    change to test

--- coach_mac_training/main.py
+++ coach_mac_training/main.py
@@ -87,24 +87,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.include_router(result_router)
-# app.include_router(athlete_router)
-# app.include_router(team_router)
-# app.include_router(record_router)
-# app.include_router(workout_router)
-# app.include_router(meetday_router)
-# # app.include_router(result_html_router)
-# # app.include_router(athlete_html_router)
-# # app.include_router(team_html_router)
-# # app.include_router(record_html_router)
-# # app.include_router(workout_html_router)
-# # app.include_router(group_html_router)
-# app.include_router(unimplemented_html_router)
-# # app.include_router(dev_html_router)
-# app.include_router(html_routs_main_router)
-
-
-
 @app.on_event("startup")
 async def startup_event():
     # db = DatabaseHandler()
